a434053997478430885f149c3820a1e036c156e5
MICHAEL EDUARDO MALDONADO PEREIRA <memaldonado28@utpl.edu.ec>
2021-06-24 16:33:32 -0500

    AAaaa

--- TrabajoGrupal-T11-B2/src/paquete2/Menu.java
+++ TrabajoGrupal-T11-B2/src/paquete2/Menu.java
@@ -10,6 +10,38 @@ package paquete2;
  * 
  * @author josef
  */
-public class Menu {
-
+public  abstract class Menu {
+  
+    protected String nombrePlato;
+    protected double valorMenu;
+    
+    public Menu (String nombre, double cuotaB){
+        establecerNombreArrendatario(nombre);
+        cuotaBase = cuotaB;
+    }
+    //************************
+    public void establecerNombreArrendatario(String nombre){
+        nombreArrendatario = nombre;
+    }
+    
+    public void establecerCuotaBase(double x){
+        cuotaBase = x;
+    }
+    
+    public abstract void establecerArriendoMensual();
+    
+    public String obtenerNombreArrendatario(){
+        return nombreArrendatario;
+    }
+    
+    public double obtenerCuotaBase(){
+        return cuotaBase;
+    }
+    
+    public double obtenerArriendoMensual(){
+        return arriendoMensual;
+    }
+       
 }
+
+
