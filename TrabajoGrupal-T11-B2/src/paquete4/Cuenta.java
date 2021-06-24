fcae93665f94201999f3dd868bd9feee10d4a0df
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2021-06-24 17:11:38 -0500

    B

--- TrabajoGrupal-T11-B2/src/paquete4/Cuenta.java
+++ TrabajoGrupal-T11-B2/src/paquete4/Cuenta.java
@@ -3,31 +3,81 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package paquete4;
 
 import java.util.ArrayList;
 import paquete2.Menu;
 
 /**
- * 
+ *
  * @author josef
  */
 public class Cuenta {
-    
+
     private String nombreCliente;
-    private double iva;
+    private double ivaPor;
+    private double ivaVal;
+    private ArrayList<Menu> lista = new ArrayList<>();
     private double subtotal;
     private double total;
-    private ArrayList<Menu> lista = new ArrayList<>();
-    
-    public Cuenta(String n, double i){
+
+    public Cuenta(String n, double i, ArrayList<Menu> l) {
         nombreCliente = n;
-        iva = i;
+        ivaPor = i;
+        lista = l;
+//        establecerIvaVal(i);
     }
-    
-    public void establecerNombreCliente(String n){
+
+    public void establecerNombreCliente(String n) {
         nombreCliente = n;
+    }
+
+    public void establecerIvaPor(double i) {
+        ivaPor = i;
+    }
+
+    public void establecerIvaVal(double i) {
+        ivaVal = (subtotal * ivaPor) / 100;
+    }
+
+    public void establecerLista(ArrayList<Menu> l) {
+        lista = l;
+    }
+
+    public void establecerSubtotal() {
+        double suma = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            suma = suma + lista.get(i).obtenerValorDelMenu();
+        }
+        subtotal = suma;
+    }
+
+    public void establecerTotal() {
+        total = subtotal + ivaVal;
+    }
+
+    public String obtenerNombreCliente() {
+        return nombreCliente;
+    }
+
+    public double obtenerIvaPor() {
+        return ivaPor;
+    }
+
+    public double obtenerIvaVal() {
+        return ivaVal;
+    }
+
+    public ArrayList<Menu> obtenerLista() {
+        return lista;
+    }
+
+    public double obtenerSubtotal() {
+        return subtotal;
+    }
+
+    public double obtenerTotal() {
+        return total;
     }
 
 }
